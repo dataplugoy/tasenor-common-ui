be4f40e87e6a744ec2a23a859ae9ca13c71d2453
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-08-16 18:26:14 +0300

    Rebuild.

--- dist/risp/Account.d.ts
+++
@@ -1,3 +0,0 @@
-import { Renderer } from 'react-interactive-stateful-process';
-import { TasenorSetup, AccountElement } from '@dataplug/tasenor-common';
-export declare const AccountRenderer: Renderer<TasenorSetup, AccountElement>;
