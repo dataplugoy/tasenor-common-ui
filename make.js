09aa9b0670715b248d9709b0335967d9c933b50d
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-08-28 17:28:12 +0300

    Switch to esbuild.

---
+++ make.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
+const isDev = process.argv.filter(a => a === '--dev').length > 0
+const isBrowser = process.argv.filter(a => a === '--browser').length > 0
+
+const pkg = JSON.parse(require('fs').readFileSync('./package.json'))
+
+async function run() {
+  await require('esbuild').build({
+    bundle: true,
+    define: isBrowser
+      ? {
+          global: 'window'
+        }
+      : {},
+    entryPoints: ['src/index.ts'],
+    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+    incremental: isDev,
+    inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
+    minify: true,
+    outfile: 'dist/index.js',
+    platform: isBrowser ? 'browser' : 'node',
+    plugins: isBrowser
+      ? [
+          require('node-stdlib-browser/helpers/esbuild/plugin')(require('node-stdlib-browser'))
+        ]
+      : [
+        ],
+    sourcemap: 'external',
+    watch: isDev && {
+      onRebuild(error, result) {
+        if (error) console.error('Watch build failed:', error)
+        else console.log('Watch build succeeded:', result)
+      },
+    },
+  }).catch((err) => { console.error(err); process.exit(1) })
+}
+
+run()
