ddafc27d0d53ea47f4305efeecf08f54bf271914
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-11-09 08:58:12 +0200

    Compile.

--- dist/bookkeeper/TabNav.d.ts
+++ dist/bookkeeper/TabNav.d.ts
@@ -1,14 +1,13 @@
-import React from 'react';
 export interface TabPanelProps {
-    children: React.Element;
+    children: JSX.Element;
     value: number;
     index: number;
 }
-export declare const TabPanel: (props: TabPanelProps) => React.Element;
+export declare const TabPanel: (props: TabPanelProps) => JSX.Element;
 export interface TabsProps {
     menu: string;
     labels: Record<string, string>;
-    children: React.Element[];
+    children: JSX.Element[];
 }
 /**
  * Tab navigation using the navigation hook directly.
@@ -17,4 +16,4 @@ export interface TabsProps {
  * @param props.labels Mapping from menu values to tab display names.
  * @returns
  */
-export declare const TabNav: (props: TabsProps) => React.Element;
+export declare const TabNav: (props: TabsProps) => JSX.Element;
